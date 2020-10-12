4251ca9287cea5b19e78a198e0d8698e53f57032
Amit Israeli <55833436+amit154154@users.noreply.github.com>
2020-10-12 03:03:10 +0300

    add crop image, need to do:
    1. add google zoom to gov zoom convator
    2. make crop even to both map types
    3. wgs84 coordinates to lat lon python

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import threading
 import time
 from pyproj import Proj, transform
 import os
+import pyproj
+from PIL import Image
+
+
 
 
 def set_driver(url):
@@ -24,15 +28,20 @@ def LatLon_to_xy(lat,lon):
 
 def xy_to_LatLon(x,y):
     #xyn to Lat Lon convator, googlemaps - Lat Lon , gov - xy
-    p = Proj(proj='WGS84',zone=10,ellps='utm', preserve_units=False)
-    lat,lon = p(x,y)
-    return lat,lon
+    # define the projections
+    p1 = Proj(init='epsg:28992')
+    p2 = Proj(proj='latlong', datum='WGS84')
+
+    # Transform point (155000.0, 446000.0) with EPSG:28992
+    lon, lat, z = transform(p1, p2, 155000.0, 446000.0, 0.0)
+    return lon, lat  # 5.38720294616 52.0023756348 43.6057764404
 
 def get_image_gov(cords,zoom):
     if(check_exist_cords('gov',cords) == False):
         url = 'https://govmap.gov.il/?c={cord_x},{cord_y}&z={z}&b=1&sb=8'.format(cord_x=cords[0],cord_y=cords[1],z = zoom)
         driver = set_driver(url)
-        image_name = str(cords) + '.jpg'
+        image_name = str(cords) + '.png'
+        image_path = 'data/gov/'+image_name
         xpath_zoom_in = '/html/body/div[1]/div[4]/button[1]'
         element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath_zoom_in)))
         time.sleep(3)
@@ -40,18 +49,41 @@ def get_image_gov(cords,zoom):
         time.sleep(1)
         element.click()
         time.sleep(1.4)
-        driver.save_screenshot('data/gov/'+image_name)
+        driver.save_screenshot(image_path)
         driver.close()
+        crop_image(image_path,'gov')
     else:
         print('skip')
 
-def get_image_googleMaps(cords,zoom):
-    if(check_exist_cords('google',cords) == False):
-        url = 'https://www.google.com/maps/@{cord_x},{cord_y},{meter_zoom}m/data=!3m1!1e3'.format(cord_x = cords[0],cord_y = cords[1],meter_zoom = zoom)
+def crop_image(image_path,map_type):
+    img = Image.open(image_path)
+    width, height = img.size
+    print(width,height)
+    left = 350
+    top = 100
+    right = 850
+    bottom = 600
+
+    im1 = img.crop((left, top, right, bottom))
+    print(im1.size)
+    #os.remove(image_path)
+    #img.save(image_path, 'png')
+    im1.show()
+
+
+
+
+def get_image_googleMaps(cords,zoom,istest = False):
+    if(check_exist_cords('google',cords) == False or istest):
+        fix_cords = xy_to_LatLon(cords[0],cords[1])
+        url = 'https://www.google.com/maps/@{cord_x},{cord_y},{meter_zoom}m/data=!3m1!1e3'.format(cord_x = fix_cords[0],cord_y = fix_cords[1],meter_zoom = str(zoom))
+        print(url)
         driver = set_driver(url)
-        image_name = str(cords) + '.jpg'
+        image_name = str(cords) + '.png'
         wait = WebDriverWait(driver, 10)
-        wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="runway-expand-button"]/div/div')))
+        time.sleep(2)
+        wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="runway-expand-button"]/div/div')))
+        time.sleep(2)
         driver.save_screenshot('data/google maps/'+image_name)
         driver.close()
     else:
@@ -101,11 +133,9 @@ def check_exist_cords(map_type,cords):
 
     all_cords = os.listdir("./data/"+dir)
     for file in all_cords:
-        if(file.split('.jpg')[0] == str(cords)):
+        if(file.split('.png')[0] == str(cords)):
             return True
     return False
-
-
 
 def start_threads(threads_num,map_type,startcords,endcords,images_num,zoom):
     thread_function = get_images_cords
@@ -127,17 +157,19 @@ def start_threads(threads_num,map_type,startcords,endcords,images_num,zoom):
         thread.join()
         logging.info("Main    : thread %d done", index)
 
-def main():
+def t():
     threds_num = 2
     image_count = 10
-    map_type = 'gov'
-    zoom = 8
+    map_type = 'google'
+    zoom = 500
 
     cords_start = [180485.11,634667.74]
     cords_end = [180685.11,634697.74]
 
     start_threads(threds_num, map_type, cords_start, cords_end, image_count, zoom)
 
+def main():
+    crop_image('/Users/amitisraeli/PycharmProjects/google_maps/data/google maps/[31.8825159, 34.7399199].jpg','google')
+
 if __name__ == '__main__':
     main()
-
